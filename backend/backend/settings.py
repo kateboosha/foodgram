4b5b737bc6063d010104263db67f333bbebbb884
Kate Borzenkova <dlya_kati17@mail.ru>
2024-10-21 12:29:58 +0400

    dockerhub

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -68,8 +68,8 @@ AUTH_USER_MODEL = 'foodgram.User'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': os.getenv('POSTGRES_DB', 'postgres'),
-        'USER': os.getenv('POSTGRES_USER', 'postgres'),
+        'NAME': os.getenv('POSTGRES_DB', 'django'),
+        'USER': os.getenv('POSTGRES_USER', 'django'),
         'PASSWORD': os.getenv('POSTGRES_PASSWORD', ''),
         'HOST': os.getenv('DB_HOST', ''),
         'PORT': os.getenv('DB_PORT', 5432)
@@ -116,12 +116,12 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
+STATIC_URL = '/static/'
+STATIC_ROOT = BASE_DIR / 'backend_static' / 'static'
+
 MEDIA_URL = '/media/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_ROOT = BASE_DIR / 'media'
 
-STATIC_URL = '/static/django/'
-
-STATIC_ROOT = os.path.join(BASE_DIR, 'static_django')
 
 LANGUAGE_CODE = 'en-us'
 
