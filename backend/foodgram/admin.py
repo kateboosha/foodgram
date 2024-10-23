99dcc956e73eafdd82a69600c139195656fa4f8a
Kate Borzenkova <dlya_kati17@mail.ru>
2024-10-23 21:38:45 +0400

    1st reviw fix but readme

--- backend/foodgram/admin.py
+++ backend/foodgram/admin.py
@@ -1,6 +1,5 @@
 from django.contrib import admin
 from django.contrib.auth import get_user_model
-from django.contrib.auth.models import Group
 
 from .models import (Favorite, Ingredient, Recipe, RecipeIngredient,
                      ShoppingCart, Tag)
