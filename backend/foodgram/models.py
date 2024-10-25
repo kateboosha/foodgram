fcf815df5af888487466ebbdae85d2514ce10364
Kate Borzenkova <dlya_kati17@mail.ru>
2024-10-25 16:20:49 +0400

    models fixed

--- backend/foodgram/models.py
+++ backend/foodgram/models.py
@@ -21,22 +21,21 @@ class User(AbstractUser):
     )
     first_name = models.CharField(max_length=FIRST_NAME_MAX_LENGTH)
     last_name = models.CharField(max_length=LAST_NAME_MAX_LENGTH)
-    avatar = models.ImageField(upload_to="avatars/", blank=True, null=True)
-    is_subscribed = models.BooleanField(default=False)
+    avatar = models.ImageField(upload_to='avatars/', blank=True, null=True)
 
-    USERNAME_FIELD = "email"
-    REQUIRED_FIELDS = ("username", "first_name", "last_name")
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = ('username', 'first_name', 'last_name')
 
     class Meta:
-        ordering = ("username",)
+        ordering = ('username',)
 
 
 class Subscription(models.Model):
     user = models.ForeignKey(
-        User, on_delete=models.CASCADE, related_name="subscriptions"
+        User, on_delete=models.CASCADE, related_name='subscriptions'
     )
     subscribed_to = models.ForeignKey(
-        User, on_delete=models.CASCADE, related_name="subscribers"
+        User, on_delete=models.CASCADE, related_name='subscribers'
     )
 
     class Meta:
@@ -50,29 +49,28 @@ class Subscription(models.Model):
                 name='prevent_self_subscription'
             ),
         ]
-        ordering = ("user",)
+        ordering = ('user',)
 
 
 class Tag(models.Model):
     name = models.CharField(max_length=TAG_NAME_MAX_LENGTH, unique=True)
     slug = models.SlugField(max_length=TAG_SLUG_MAX_LENGTH, unique=True)
 
+    class Meta:
+        ordering = ('name',)
+
     def __str__(self):
         return self.name
-
-    class Meta:
-        ordering = ("name",)
 
 
 class Ingredient(models.Model):
     name = models.CharField(
         max_length=INGREDIENT_NAME_MAX_LENGTH,
-        unique=False
     )
     measurement_unit = models.CharField(max_length=MEASUREMENT_UNIT_MAX_LENGTH)
 
     def __str__(self):
-        return f"{self.name} ({self.measurement_unit})"
+        return f'{self.name} ({self.measurement_unit})'
 
     class Meta:
         constraints = [
@@ -81,32 +79,30 @@ class Ingredient(models.Model):
                 name='unique_ingredient_name_unit'
             )
         ]
-        ordering = ("name",)
+        ordering = ('name',)
 
 
 class Recipe(models.Model):
     author = models.ForeignKey(
         User,
-        related_name="recipes",
+        related_name='recipes',
         on_delete=models.CASCADE
     )
     name = models.CharField(max_length=RECIPE_NAME_MAX_LENGTH)
-    image = models.ImageField(upload_to="recipes/")
+    image = models.ImageField(upload_to='recipes/')
     text = models.TextField()
     ingredients = models.ManyToManyField(
-        Ingredient, through="RecipeIngredient", related_name="recipes"
+        Ingredient, through='RecipeIngredient', related_name='recipes'
     )
-    tags = models.ManyToManyField(Tag, related_name="recipes")
+    tags = models.ManyToManyField(Tag, related_name='recipes')
     cooking_time = models.PositiveIntegerField(
         validators=[MinValueValidator(1)]
     )
-    # is_favorited = models.BooleanField(default=False)
-    # is_in_shopping_cart = models.BooleanField(default=False)
     created_at = models.DateTimeField(auto_now_add=True)
     short_link_hash = models.CharField(max_length=6, unique=True, blank=True)
 
     class Meta:
-        ordering = ("-created_at",)
+        ordering = ('-created_at',)
 
     def __str__(self):
         return self.name
@@ -121,59 +117,3 @@ class Recipe(models.Model):
         if not self.short_link_hash:
             self.short_link_hash = self.generate_short_link()
         super().save(*args, **kwargs)
-
-
-class RecipeIngredient(models.Model):
-    recipe = models.ForeignKey(
-        Recipe, related_name="recipe_ingredients", on_delete=models.CASCADE
-    )
-    ingredient = models.ForeignKey(
-        Ingredient, related_name="ingredient_recipes", on_delete=models.CASCADE
-    )
-    amount = models.PositiveIntegerField()
-
-    class Meta:
-        constraints = [
-            models.UniqueConstraint(
-                fields=("recipe", "ingredient"),
-                name="unique_recipe_ingredient"
-            )
-        ]
-        ordering = ("recipe",)
-
-
-class UserRecipeBase(models.Model):
-    user = models.ForeignKey(
-        settings.AUTH_USER_MODEL,
-        on_delete=models.CASCADE
-    )
-    recipe = models.ForeignKey(
-        Recipe,
-        on_delete=models.CASCADE
-    )
-
-    class Meta:
-        abstract = True
-        constraints = [
-            models.UniqueConstraint(
-                fields=["user", "recipe"],
-                name="unique_%(class)s_user_recipe"
-            )
-        ]
-
-
-class Favorite(UserRecipeBase):
-    class Meta:
-        default_related_name = "favorites"
-
-    def __str__(self):
-        return f'{self.user.email} добавил {self.recipe.name} в избранное'
-
-
-class ShoppingCart(UserRecipeBase):
-    class Meta:
-        db_table = "shopping_cart"
-        default_related_name = "shopping_cart"
-
-    def __str__(self):
-        return f'{self.user.email} добавил {self.recipe.name} в корзину'
